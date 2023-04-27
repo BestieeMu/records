47344c860e07df22bc250a2dc4c3d0d889e9e5a2
BestieeMu <chideraigboka7@gmail.com>
2023-04-27 14:34:24 -0700

    first commit

---
+++ src/Components/navbar.tsx
@@ -0,0 +1,54 @@
+
+import Image from "next/image";
+import LogoIcon from '../assets/logo.png'
+import { auth } from "../../firebase/clientApp";
+import { signOut } from "firebase/auth";
+import { Toaster, toast } from "react-hot-toast";
+const NavBar = () => {
+
+  const logOut = (e: any) =>{
+e.preventDefault()
+signOut(auth).then(() => {
+  toast.success('log out user successful')
+
+}).catch((error) => {
+   alert('error loging out user')
+});
+
+  }
+
+  return (
+   <>
+    <div className="bg-white h-12 flex items-center justify-between w-full fixed top-0">
+      {/* logo */}
+      <div className="flex items-center gap-5">
+      <div className="bg-white h-12 line-nav w-14 flex items-center justify-center">
+      <div className='w-12 h-12 bg-white flex items-center justify-center' >
+      <Image
+        src={LogoIcon}
+        alt={'alt'}
+        
+      />
+</div>
+      </div>
+<div className="w-72 font-bold flex items-center rounded">
+<h1>Device Records</h1>
+</div>
+      </div>
+
+{/* profile */}
+      <div className="flex items-center gap-3 bg-white h-12 mr-14">
+<div className='w-10 h-10 bg-white flex items-center h-12 justify-center'>
+</div>
+<button className="p-1 w-32 bg-red-500 rounded text-white" onClick={logOut}>log out</button>
+      </div>
+      <Toaster
+  position="top-center"
+  reverseOrder={false}
+/>
+    </div>
+   </>
+  )
+}
+
+export default NavBar
